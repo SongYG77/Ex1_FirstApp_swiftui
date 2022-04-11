aa973b53b5bc53875b25273257b403a930216885
SongYunGeun <rjawoakstp@naver.com>
2022-04-11 17:44:32 +0900

    Initial Commit

---
+++ Ex1_FirstApp_swiftui/Ex1_FirstApp_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex1_FirstApp_swiftuiApp.swift
+//  Ex1_FirstApp_swiftui
+//
+//  Created by 송윤근 on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct Ex1_FirstApp_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
